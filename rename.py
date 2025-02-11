8881f7da9f25b152a7ea27f1a230371d3a156a44
Ivanovswork <destira@mail.ru>
2025-02-11 06:52:54 +0300

    Removing unnecessary function

--- rename.py
+++ rename.py
@@ -17,13 +17,6 @@ def png_in_name(txt_path):
     return txt_path.count('_png.') != 0 or txt_path.count('_PNG.')
 
 
-def rename(txt_path, f_path):
-    new_f_path = txt_path.replace('_txt', '_jpg')
-    new_f_path = os.path.join(new_bb_dir, new_f_path)
-    os.rename(f_path, new_f_path)
-    return new_f_path
-
-
 if __name__ == "__main__":
     for f in os.listdir(bb_dir):
         f_path = os.path.join(bb_dir, f)
@@ -31,21 +24,17 @@ if __name__ == "__main__":
         print(f_path, new_f_path)
         if os.path.getsize(f_path) != 0 and is_wrong(f):
             new_f_path = os.path.join(new_bb_dir, new_f_path)
-            print("The file name has been changed: old file path - " + f_path
-                  + " | new file path " + new_f_path)
             shutil.copy(f_path, new_f_path)
             print("The file name has been changed: old file path - " + f_path
                   + " | new file path " + new_f_path)
         elif os.path.getsize(f_path) == 0 and is_wrong(f):
             right_f_path = os.path.join(bb_dir, new_f_path)
             new_f_path = os.path.join(new_bb_dir, new_f_path)
-            print("The file name has been changed: old file path - " + right_f_path
-                  + " | new file path " + new_f_path)
             shutil.copy(right_f_path, new_f_path)
             print("The file name has been changed: old file path - " + right_f_path
                   + " | new file path " + new_f_path)
         elif png_in_name(f):
             new_f_path = os.path.join(new_bb_dir, f)
+            shutil.copy(f_path, new_f_path)
             print("The file name has been changed: old file path - " + f_path
                   + " | new file path " + new_f_path)
-            shutil.copy(f_path, new_f_path)
